8b87f179666c2cfe349619a8d88ce109a1f1a709
Trang Ho <carepjan@gmail.com>
2013-12-26 21:43:25 +0700

    Init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TechTalkParallax::Application.config.secret_key_base = '71120db0432a72955150d2f30fcce720d8aa6d1103c3add29a8447118d10bdbbfaaf02ff9ae4a2f16be0cfddf45b27a726dfa3573ad66e9e158d195dbbd8bdad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TechTalkParallax::Application.config.session_store :cookie_store, key: '_tech_talk_parallax_session'
